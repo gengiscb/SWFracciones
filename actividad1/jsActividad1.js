624e14d936125eabd6284a445207637b5bc6c914
Gengis Cetina <gengiscb@gmail.com>
2013-02-23 18:38:38 -0600

    Codigo fuente de la actividad 1

---
+++ actividad1/jsActividad1.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function animateImage() {
+            console.log("Called");
+            $('#bee').css({right:'10%'});   
+            $('#bee').animate({right: '-100%'}, 5000, 'linear');
+        }
+         
